9c763e14abce035cad2a56b37d2d7f1b9ded84e1
hiruni500 <hiruniimalsha12@gmail.com>
2025-05-06 08:11:57 +0530

    create s-navbar.js

---
+++ frontend/src/pages/Inventory Management/S_NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../assets/css/InventoryMangement/S_navBar.css'
+// import logo from '../Images/selyn-high-resolution-logo-transparent.png'
+function NavBar(){
+    return(
+        
+        <div class="navbar-details">
+          <a href="/addservice">He</a>
+          <a href="/">All Inventory</a>
+          
+        
+          
+        </div>
+        
+             
+            )
+}
+export default NavBar
